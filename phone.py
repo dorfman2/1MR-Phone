422caa7239d78857da403b28398cfd310eac8603
dorfman2 <dorfman2@users.noreply.github.com>
2017-03-20 22:04:59 -0400

    Update phone.py

--- phone.py
+++ phone.py
@@ -50,7 +50,7 @@ GPIO.add_event_detect(24, GPIO.BOTH)
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--ip", default="10.1.10.18", help="The ip of the OSC server")
-	parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")
+	parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
 	args = parser.parse_args()
 
 	client = udp_client.SimpleUDPClient(args.ip, args.port)
