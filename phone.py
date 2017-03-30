523fe149c9e1a5333b31e8d116dba9af8f4f2f99
dorfman2 <dorfman2@users.noreply.github.com>
2017-03-29 22:43:24 -0400

    Fixed OSC module and added dial tone

--- phone.py
+++ phone.py
@@ -2,8 +2,8 @@
 
 # 1 Mile Radius Telephone is an interactive rotary telephone created for the 1 Mile Radius Project.
 # Written by Jeffrey Dorfman, and using code from Ian Shelanskey and Simon Jenny.
-# VERSION 1.1.1
-# 19 March, 2017
+# VERSION 1.1.2
+# 29 March, 2017
 
 
 import RPi.GPIO as GPIO  
@@ -12,6 +12,7 @@ import subprocess
 import socket
 import argparse
 import random
+
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
@@ -27,6 +28,8 @@ GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Normally Open Reciever Switc
 c=0
 last = 1
 dialednum = 0
+reciever = 0 # 0 is pressed, 1 is released
+startup = 0
 
 def count(pin):
 	global c 
@@ -42,19 +45,24 @@ call(["amixer", "cset", "numid=3", "0"])
 # Above adjusts volume of amixer to 100%, this line may vary depending on version. Use "amixer controls" to discover your numid=?
 # Also ensures correct output, mine kept defaulting to the wrong one.
 
-GPIO.add_event_detect(18, GPIO.BOTH)
-GPIO.add_event_detect(24, GPIO.BOTH)
-
 # ===== MAIN SCRIPT =====
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--ip", default="10.1.10.18", help="The ip of the OSC server")
+	parser.add_argument("--ip", default="192.168.1.220", help="The ip of the OSC server")
 	parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
 	args = parser.parse_args()
 
 	client = udp_client.SimpleUDPClient(args.ip, args.port)
 # Above is for OSC Protocols
+
+
+GPIO.add_event_detect(18, GPIO.BOTH)
+GPIO.add_event_detect(24, GPIO.BOTH)
+
+reciever = GPIO.input(24)
+
+# Above is to set GPIO Pins
 
 while True:
 	try:
@@ -77,41 +85,33 @@ while True:
 					GPIO.remove_event_detect(23)
 					number = math.floor(c/2.1)
 					dialednum = str(number)
-					player = subprocess.Popen(["mpg123", "/media/" + dialednum + ".mp3", "-q"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
-					if(dialednum == 0):
-						client.send_message(["/cue/0/fire"], random.random())
-					elif(dialednum == 1):
-						client.send_message(["/cue/1/fire"], random.random()) 
-					elif(dialednum == 2):
-						client.send_message(["/cue/2/fire"], random.random())
-					elif(dialednum ==3):
-						client.send_message(["/cue/3/fire"], random.random())
-					elif(dialednum == 4):
-						client.send_message(["/cue/4/fire"], random.random()) 
-					elif(dialednum == 5):
-						client.send_message(["/cue/5/fire"], random.random())
-					elif(dialednum == 5):
-						client.send_message(["/cue/6/fire"], random.random())
-					elif(dialednum == 6):
-						client.send_message(["/cue/7/fire"], random.random()) 
-					elif(dialednum == 7):
-						client.send_message(["/cue/8/fire"], random.random())
-					elif(dialednum == 8):
-						client.send_message(["/cue/9/fire"], random.random())
-					elif(dialednum == 9):
-						client.send_message(["/cue/10/fire"], random.random()) 
-
+					player = subprocess.Popen(["mpg123", "-q", "-@", "/media/" + dialednum + ".m3u"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					client.send_message("cue/" + dialednum + "/fire", dialednum)
 					c=0
 					
 				last = GPIO.input(18)
 				
 		if GPIO.event_detected(24):
+			reciever = GPIO.input(24)
+			if(startup == 0):
+				player = subprocess.Popen(["mpg123", "-q", "/media/dialtone.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				startup = 1
+			if(reciever == 0):
+				try:
+					player.kill()
+					client.send_message("cue/hangup", 0)
+				except NameError:
+					pass
+
+			if(reciever == 1):
+		
+				try:
+					player.kill()
+					player = subprocess.Popen(["mpg123", "-q", "/media/dialtone.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					client.send_message("cue/dialtone", 0)
+				except NameError:
+					pass
 			
-			try:
-				player.kill()
-				client.send_message(["/cue/stop"], random.random())
-			except NameError:
-				pass
-			
+		
 	except KeyboardInterrupt:
 		break
