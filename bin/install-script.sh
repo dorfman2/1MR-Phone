6c8dedb7c62994ec8f896062a56bc9ace78d496e
dorfman2 <dorfman2@users.noreply.github.com>
2018-03-21 21:50:04 -0400

    added sudo

--- bin/install-script.sh
+++ bin/install-script.sh
@@ -2,4 +2,4 @@
 sudo chmod x /home/pi/1MR-Phone/phone.py
 
 # This is to append rc.local to start phone on boot
-sed -i -e '$i \/home/pi/1MR-Phone/./phone &\n' rc.local
+sudo sed -i -e '$i \/home/pi/1MR-Phone/./phone &\n' /etc/rc.local
