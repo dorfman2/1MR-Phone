fd859d03478bf68847d986fc146121af302a0c03
dorfman2 <dorfman2@users.noreply.github.com>
2018-03-21 22:25:39 -0400

    Update install-script.sh

--- bin/install-script.sh
+++ bin/install-script.sh
@@ -1,5 +1,5 @@
 # Make phone.py executable
-sudo chmod x /home/pi/1MR-Phone/phone.py
+sudo chmod -x /home/pi/1MR-Phone/phone.py
 
 # This is to append rc.local to start phone on boot
 sudo sed -i -e '$i \/home/pi/1MR-Phone/./phone &\n' /etc/rc.local
