5f0f3e1c41b9e9ed9407f2f127ad8b48306da8d7
dorfman2 <dorfman2@buffalo.edu>
2018-05-08 15:42:23 -0400

    Fixed track_count error, changed Record trigger to 0, added track_start to config.ini

--- phone.py
+++ phone.py
@@ -37,6 +37,7 @@ config.read('config.ini')
 phone_id = config.get('phone', 'id')
 osc_enable = config.get('phone', 'osc')
 track_limit = int(config.get('phone', 'track_limit'))
+track_count = int(config.get('phone', 'track_start'))
 
 
 osc_ip = config.get('osc', 'ip')
@@ -54,7 +55,7 @@ bouncetime_rotary = float(config.get('bouncetime', 'rotary'))
 bouncetime_hook = float(config.get('bouncetime', 'hook'))
 
 
-subprocess.Popen(["amixer -q set Speaker 100%"], shell=True) # Sets the volume to 0db (maximum)
+subprocess.Popen(["amixer -q set Speaker 80%"], shell=True) # Sets the volume to 0db (maximum)
 subprocess.Popen(["amixer -q set Mic 25%"], shell=True)
 #subprocess.Popen(["amixer -q set 'Auto Gain Control' on"], shell=True)
 
@@ -67,8 +68,7 @@ subprocess.Popen(["amixer -q set Mic 25%"], shell=True)
 
 class Microphone():
     rec_subprocess = None
-    track_count = 0
-    track_limit = 0
+
 
     def get_track_name(self):
         dir_name = "/home/pi/1MR-Phone/media"
@@ -106,12 +106,12 @@ class Microphone():
 
 
 class Dial():
-    def __init__(self, track_limit):
+    def __init__(self, track_count, track_limit):
         self.pulses = 0
         self.number = ""
         self.counting = True
         self.calling = False
-        self.microphone = Microphone(track_limit)
+        self.microphone = Microphone(track_count, track_limit)
 
 
     def startcalling(self):
@@ -153,7 +153,7 @@ class Dial():
                 self.shutdown()
                 return
             
-            if self.number == "7867": #If you dial "STOP" restarts
+            if self.number == "737": #If you dial "RES" restarts
                 try:
                     self.player.kill()
                 except:
@@ -162,12 +162,13 @@ class Dial():
                 self.restart()
                 return
             
-            if self.number == "732": #start recording audio
+            if self.number == "0": #start recording audio
                 try:
                     self.player.kill()
                 except:
                     pass
-                
+                self.player = subprocess.Popen(["aplay", "-q", "--device=plughw:1,0", "/home/pi/1MR-Phone/media/leave_message.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                time.sleep(4)
                 self.microphone.recordStart()
                 return
             
@@ -209,14 +210,14 @@ class Dial():
         self.pulses = 0
         self.number = ""
 
-    def shutdown():
-        subprocess.Popen(["aplay", "-q", "--device=plughw:1,0", "/home/pi/1MR-Phone/media/shutdown.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def shutdown(self):
+        subprocess.Popen(["aplay", "-q", "--device=plughw:1,0", "/home/pi/1MR-Phone/media/shutdown.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(5)
         subprocess.Popen(["sudo shutdown -h now"], shell=True)
         
         
-    def restart():
-        subprocess.Popen(["aplay", "-q", "--device=plughw:1,0", "/home/pi/1MR-Phone/media/restart.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def restart(self):
+        subprocess.Popen(["aplay", "-q", "--device=plughw:1,0", "/home/pi/1MR-Phone/media/restart.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(5)
         subprocess.Popen(["sudo reboot"], shell=True)
             
@@ -246,7 +247,7 @@ if __name__ == "__main__":
     countrotary = gpiozero.DigitalInputDevice(pin_countrotary, pull_up=True, bounce_time=bouncetime_rotary)
     hook = gpiozero.DigitalInputDevice(pin_hook, pull_up=True, bounce_time=bouncetime_hook)
 
-    dial = Dial(track_limit=track_limit)
+    dial = Dial(track_count=track_count, track_limit=track_limit)
     countrotary.when_deactivated = dial.addpulse
     countrotary.when_activated = dial.addpulse
     rotaryenable.when_activated = dial.startcounting
